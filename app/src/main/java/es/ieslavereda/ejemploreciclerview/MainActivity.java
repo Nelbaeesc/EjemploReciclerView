cad8ef87bad326f21b57d4d14e7dba54eae58af2
Nelson Baez <nelbaeesc@alu.edu.gva.es>
2023-03-22 21:19:34 +0100

    a

--- app/src/main/java/es/ieslavereda/ejemploreciclerview/MainActivity.java
+++ app/src/main/java/es/ieslavereda/ejemploreciclerview/MainActivity.java
@@ -1,14 +1,26 @@
 package es.ieslavereda.ejemploreciclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler);
+
+        MyRecyclerViewAdapter myRecyclerViewAdapter = new MyRecyclerViewAdapter(this);
+        recyclerView.setAdapter(myRecyclerViewAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
     }
 }
